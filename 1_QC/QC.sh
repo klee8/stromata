235d2504e0cb668d26f1a6489e4dd78f3df3bb31
kate <K.Lee3@massey.ac.nz>
2019-03-21 12:56:21 +1300

    basic qc analysis

---
+++ 1_QC/QC.sh
@@ -0,0 +1,9 @@
+# QC reads using SolexaQA++ v3.1.7.1
+# usage info at http://solexaqa.sourceforge.net/
+#################################################
+
+#!usr/bin/bash
+
+
+# ran basic analysis to see read quality
+SolexaQA++ analysis /opt/stroma_RNAseq/Ee-PS1_S7_L007_R1_001.fastq.gz -d Ee_SolexaQC/Ee_PS1_S7_L007_R1_001
