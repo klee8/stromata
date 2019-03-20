1de14ebe9df04e059ce6ce4373491bb956499cac
kate <K.Lee3@massey.ac.nz>
2019-03-21 12:57:46 +1300

    reads look qc'd already

--- 1_QC/QC.sh
+++ 1_QC/QC.sh
@@ -7,3 +7,6 @@
 
 # ran basic analysis to see read quality
 SolexaQA++ analysis /opt/stroma_RNAseq/Ee-PS1_S7_L007_R1_001.fastq.gz -d Ee_SolexaQC/Ee_PS1_S7_L007_R1_001
+
+
+# File looks like it has already been trimmed, skip this step and go straight to mapping
