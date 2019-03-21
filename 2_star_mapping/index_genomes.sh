a6c76977585e65b6687394f47f88088edaf5ea53
kate <K.Lee3@massey.ac.nz>
2019-03-21 16:46:49 +1300

    indexed genomes

---
+++ 2_star_mapping/index_genomes.sh
@@ -0,0 +1,8 @@
+# map reads to genome with STAR
+
+#!/usr/bin/env bash
+
+# make a genome index
+STAR --runMode genomeGenerate --runThreadN 4 --genomeDir genomes/Eel_728 --genomeLoad LoadAndKeep --genomeFastaFiles genomes/Eel_728/Eel_masked.fasta --outFileNamePrefix Eel_728_
+STAR --runMode genomeGenerate --runThreadN 4 --genomeDir genomes/Efe_2368 --genomeLoad LoadAndKeep --genomeFastaFiles genomes/Eel_728/Eel_masked.fasta --outFileNamePrefix Efe_2368_
+STAR --runMode genomeGenerate --runThreadN 4 --genomeDir genomes/Ety_8 --genomeLoad LoadAndKeep --genomeFastaFiles genomes/Eel_728/Eel_masked.fasta --outFileNamePrefix Ety_8_
