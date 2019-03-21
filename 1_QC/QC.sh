50788b3869dc97faecf8e8f554f9743d99d20f36
kate <K.Lee3@massey.ac.nz>
2019-03-21 15:23:35 +1300

    quality analysis and trimming for E.elymi second run reads

--- 1_QC/QC.sh
+++ 1_QC/QC.sh
@@ -1,12 +1,18 @@
 # QC reads using SolexaQA++ v3.1.7.1
 # usage info at http://solexaqa.sourceforge.net/
+# trimmed reads < phred 20
 #################################################
 
 #!usr/bin/bash
 
-
-# ran basic analysis to see read quality
-SolexaQA++ analysis /opt/stroma_RNAseq/Ee-PS1_S7_L007_R1_001.fastq.gz -d Ee_SolexaQC/Ee_PS1_S7_L007_R1_001
-
-
-# File looks like it has already been trimmed, skip this step and go straight to mapping
+# trimed with a cut off of < pred 20
+mkdir E.elymi
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-Inf1_S14_L006_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-Inf2_S15_L006_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-Inf3_S16_L006_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+#SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-PS1_S7_L007_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-PS2_S8_L008_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-PS3_S13_L005_R1_001_2.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-STR1_S10_L004_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-STR2_S11_L004_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
+SolexaQA++ dynamictrim /opt/stroma_RNAseq/Ee-STR3_S12_L004_R1_001.fastq.gz --phredcutoff 20 --bwa --directory E.elymi --sanger
