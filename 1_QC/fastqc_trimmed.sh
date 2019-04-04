d1d969f5ec8c2cabda9fa3716c8a4d2ee0a9ff27
kate <K.Lee3@massey.ac.nz>
2019-04-05 09:18:19 +1300

    qc scripts setup with perl script

---
+++ 1_QC/fastqc_trimmed.sh
@@ -0,0 +1,53 @@
+# fastQC of reads after trimming
+
+#!/usr/bin/env bash
+source /home/kate/.bash_profile
+mkdir fastQC_trimmed
+mkdir fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf1_S1_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf1_S1_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf2_S2_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf2_S2_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf3_S16_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf3_S3_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-Inf3_S3_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS1_S3_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS1_S3_L007_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS1_S7_L007_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS2_S4_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS2_S4_L007_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS2_S8_L008_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS3_S13_L005_R1_001_2.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS3_S5_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-PS3_S5_L007_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-STR1_S10_L004_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-STR2_S11_L004_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+fastqc trimmed/E.elymi_NfE728/Ee-STR3_S12_L004_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728
+mkdir fastQC_trimmed/E.elymi_NfE728_NfE728
+fastqc trimmed/E.elymi_NfE728_NfE728/Ee-Inf1_S14_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728_NfE728
+fastqc trimmed/E.elymi_NfE728_NfE728/Ee-Inf2_S15_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.elymi_NfE728_NfE728
+mkdir fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-Inf1_S1_L005_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-Inf1_S7_L003_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-Inf2_S8_L003_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-Inf3_S2_L005_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-Inf3_S9_L003_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-PS1_S4_L004_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-PS2_S5_L005_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-PS3_S6_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-STR1_S4_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-STR2_S5_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+fastqc trimmed/E.festucae_E2368/Ef-STR3_S6_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.festucae_E2368
+mkdir fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-PS1_S1_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-PS2_S2_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-PS3_S3_L003_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-STR1_S1_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-STR2_S2_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+fastqc trimmed/E.typhina_E8/Et-STR3_S3_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/E.typhina_E8
+mkdir fastQC_trimmed/UN
+fastqc trimmed/UN/Undetermined_S0_L001_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/UN
+fastqc trimmed/UN/Undetermined_S0_L002_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/UN
+fastqc trimmed/UN/Undetermined_S0_L005_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/UN
+fastqc trimmed/UN/Undetermined_S0_L006_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/UN
+fastqc trimmed/UN/Undetermined_S0_L007_R1_001.trim.fastq.gz --outdir=fastQC_trimmed/UN
