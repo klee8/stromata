0b4489f6a9720eb406a21649b1b97a5693d179fb
klee8 <kate.d.lee@gmail.com>
2019-08-12 10:37:37 +1200

    separated data from scripts

--- Epichloe_clusters/antismash/run_sum_antismash.sh
+++ Epichloe_clusters/antismash/run_sum_antismash.sh
@@ -1,4 +1,12 @@
 # grab antismash data from genbank file outputs
+# antismash data was obtained from the antismash website
+# whole and fragmented genomes were used
+
+
+SCRIPTDIR="/media/kate/Massey_linux_onl/projects/STROMATA/stromata_analysis/Epichloe_clusters/antismash/"
+DATADIR="/media/kate/Massey_linux_onl/projects/STROMATA/results/Epichloe_clusters/antismash/"
+
+cd $DATADIR
 
 mkdir results
 
@@ -8,7 +16,7 @@ mkdir results
 cd Eel728_ONP_polished/
 rm ../results/E.elymi_728_ONP_polished_antismash_sum.txt
 echo -e "Chr\tregion\treg_start\treg_end\tgene_name\texons\tgene_start\tgene_end\tdesc\ttranslation" > ../results/E.elymi_728_ONP_polished_antismash_sum.txt
-for i in *region00*.gbk; do perl ../get_genbank_data.pl $i >> ../results/E.elymi_728_ONP_polished_antismash_sum.txt; done
+for i in *region00*.gbk; do perl $SCRIPTDIR/get_genbank_data.pl $i >> ../results/E.elymi_728_ONP_polished_antismash_sum.txt; done
 cd ..
 
 
@@ -17,18 +25,18 @@ cd ..
 cd E.festucae_E2368/
 rm ../results/E.festucae_E2368_antismash_sum.txt
 echo -e "Chr\tregion\treg_start\treg_end\tgene_name\texons\tgene_start\tgene_end\tdesc\ttranslation" > ../results/E.festucae_E2368_antismash_sum.txt
-for i in *region00*.gbk; do perl ../get_genbank_data.pl $i >> ../results/E.festucae_E2368_antismash_sum.txt; done
+for i in *region00*.gbk; do perl  $SCRIPTDIR/get_genbank_data.pl $i >> ../results/E.festucae_E2368_antismash_sum.txt; done
 cd ..
 
 cd E.typhina_E8/
 rm ../results/E.typhina_E8_antismash_sum.txt
 echo -e "Chr\tregion\treg_start\treg_end\tgene_name\texons\tgene_start\tgene_end\tdesc\ttranslation" > ../results/E.typhina_E8_antismash_sum.txt
-for i in *region00*.gbk; do perl ../get_genbank_data.pl $i >> ../results/E.typhina_E8_antismash_sum.txt; done
+for i in *region00*.gbk; do perl  $SCRIPTDIR/get_genbank_data.pl $i >> ../results/E.typhina_E8_antismash_sum.txt; done
 cd ..
 
 cd Eel_masked/
 rm ../results/Eel_masked_antismash_sum.txt
 echo -e "Chr\tregion\treg_start\treg_end\tgene_name\texons\tgene_start\tgene_end\tdesc\ttranslation" > ../results/Eel_masked_antismash_sum.txt
-for i in *region00*.gbk; do perl ../get_genbank_data.pl $i >> ../results/Eel_masked_antismash_sum.txt; done
+for i in *region00*.gbk; do perl  $SCRIPTDIR/get_genbank_data.pl $i >> ../results/Eel_masked_antismash_sum.txt; done
 cd ..
 
