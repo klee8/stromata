d78e9bad611908144cd376c64607f4d9683a0593
klee8 <kate.d.lee@gmail.com>
2019-08-12 11:32:24 +1200

    changed data dir

--- Epichloe_clusters/id_DE_clusters/summarise_clusters_by_orthogroup.R
+++ Epichloe_clusters/id_DE_clusters/summarise_clusters_by_orthogroup.R
@@ -3,8 +3,10 @@
 
 library(tidyverse)
 
+setwd("/media/kate/Massey_linux_onl/projects/STROMATA/results/Epichloe_clusters/id_DE_clusters/")
+
 # read in ortholog flat file
-orth <- read.delim("flat_ortho_file.txt", sep = " ", header = TRUE) 
+orth <- read.delim("../../../../data/gene_model_sets/proteinortho/flat_ortho_file.txt", sep = " ", header = TRUE)
 orth$spp <- NULL
 orth$gene_id <- NULL
 orth_list <- data.frame(unique(orth$ortho_group))
@@ -64,34 +66,34 @@ inf_ps_groups <- merge(inf_ps_groups, inf_ps_ty, by = "ortho_group")
 
 
 # add group and number of species columns
-str_ps_groups$cluster_group <- ifelse(!is.na(str_ps_groups$el_group), str_ps_groups$el_group, 
-                              ifelse(!is.na(str_ps_groups$fs_group), str_ps_groups$fs_group, 
-                              ifelse(!is.na(str_ps_groups$ty_group), str_ps_groups$ty_group, "NA"))) 
-str_ps_groups$cluster_num_spp <- ifelse(!is.na(str_ps_groups$el_num_spp), str_ps_groups$el_num_spp, 
-                              ifelse(!is.na(str_ps_groups$fs_num_spp), str_ps_groups$fs_num_spp, 
-                                     ifelse(!is.na(str_ps_groups$ty_num_spp), str_ps_groups$ty_num_spp, "NA"))) 
+str_ps_groups$cluster_group <- ifelse(!is.na(str_ps_groups$el_group), str_ps_groups$el_group,
+                              ifelse(!is.na(str_ps_groups$fs_group), str_ps_groups$fs_group,
+                              ifelse(!is.na(str_ps_groups$ty_group), str_ps_groups$ty_group, "NA")))
+str_ps_groups$cluster_num_spp <- ifelse(!is.na(str_ps_groups$el_num_spp), str_ps_groups$el_num_spp,
+                              ifelse(!is.na(str_ps_groups$fs_num_spp), str_ps_groups$fs_num_spp,
+                                     ifelse(!is.na(str_ps_groups$ty_num_spp), str_ps_groups$ty_num_spp, "NA")))
 
-str_inf_groups$cluster_group <- ifelse(!is.na(str_inf_groups$el_group), str_inf_groups$el_group, 
-                                      ifelse(!is.na(str_inf_groups$fs_group), str_inf_groups$fs_group, 
-                                             ifelse(!is.na(str_inf_groups$ty_group), str_inf_groups$ty_group, "NA"))) 
-str_inf_groups$cluster_num_spp <- ifelse(!is.na(str_inf_groups$el_num_spp), str_inf_groups$el_num_spp, 
-                                        ifelse(!is.na(str_inf_groups$fs_num_spp), str_inf_groups$fs_num_spp, 
-                                               ifelse(!is.na(str_inf_groups$ty_num_spp), str_inf_groups$ty_num_spp, "NA"))) 
+str_inf_groups$cluster_group <- ifelse(!is.na(str_inf_groups$el_group), str_inf_groups$el_group,
+                                      ifelse(!is.na(str_inf_groups$fs_group), str_inf_groups$fs_group,
+                                             ifelse(!is.na(str_inf_groups$ty_group), str_inf_groups$ty_group, "NA")))
+str_inf_groups$cluster_num_spp <- ifelse(!is.na(str_inf_groups$el_num_spp), str_inf_groups$el_num_spp,
+                                        ifelse(!is.na(str_inf_groups$fs_num_spp), str_inf_groups$fs_num_spp,
+                                               ifelse(!is.na(str_inf_groups$ty_num_spp), str_inf_groups$ty_num_spp, "NA")))
 
-inf_ps_groups$cluster_group <- ifelse(!is.na(inf_ps_groups$el_group), inf_ps_groups$el_group, 
-                                      ifelse(!is.na(inf_ps_groups$fs_group), inf_ps_groups$fs_group, 
-                                             ifelse(!is.na(inf_ps_groups$ty_group), inf_ps_groups$ty_group, "NA"))) 
-inf_ps_groups$cluster_num_spp <- ifelse(!is.na(inf_ps_groups$el_num_spp), inf_ps_groups$el_num_spp, 
-                                        ifelse(!is.na(inf_ps_groups$fs_num_spp), inf_ps_groups$fs_num_spp, 
-                                               ifelse(!is.na(inf_ps_groups$ty_num_spp), inf_ps_groups$ty_num_spp, "NA"))) 
+inf_ps_groups$cluster_group <- ifelse(!is.na(inf_ps_groups$el_group), inf_ps_groups$el_group,
+                                      ifelse(!is.na(inf_ps_groups$fs_group), inf_ps_groups$fs_group,
+                                             ifelse(!is.na(inf_ps_groups$ty_group), inf_ps_groups$ty_group, "NA")))
+inf_ps_groups$cluster_num_spp <- ifelse(!is.na(inf_ps_groups$el_num_spp), inf_ps_groups$el_num_spp,
+                                        ifelse(!is.na(inf_ps_groups$fs_num_spp), inf_ps_groups$fs_num_spp,
+                                               ifelse(!is.na(inf_ps_groups$ty_num_spp), inf_ps_groups$ty_num_spp, "NA")))
 
-keep <- c("ortho_group", "el_cluster", "el_cluster_size", "el_gene_id", "el_start", "el_end", "el_strand", "fs_cluster", 
-"fs_cluster_size", "fs_gene_id", "fs_start", "fs_end", "fs_strand", "ty_cluster", "ty_cluster_size", 
+keep <- c("ortho_group", "el_cluster", "el_cluster_size", "el_gene_id", "el_start", "el_end", "el_strand", "fs_cluster",
+"fs_cluster_size", "fs_gene_id", "fs_start", "fs_end", "fs_strand", "ty_cluster", "ty_cluster_size",
 "ty_gene_id", "ty_start", "ty_end", "ty_strand", "cluster_group", "cluster_num_spp")
 
 str_ps_groups <- str_ps_groups[, keep]
 
-keep <- c("ortho_group", "el_cluster", "el_cluster_size", "el_gene_id", "el_start", "el_end", "el_strand", "fs_cluster", 
+keep <- c("ortho_group", "el_cluster", "el_cluster_size", "el_gene_id", "el_start", "el_end", "el_strand", "fs_cluster",
           "fs_cluster_size", "fs_gene_id", "fs_start", "fs_end", "fs_strand", "cluster_group", "cluster_num_spp")
 
 str_inf_groups <- str_inf_groups[, keep]
@@ -100,4 +102,3 @@ inf_ps_groups <- str_inf_groups[, keep]
 write.table(str_ps_groups, "STR_PS/str_ps_groups_wide_file_for_ann.txt", quote = FALSE, row.names = FALSE)
 write.table(str_inf_groups, "STR_INF/str_inf_groups_wide_file_for_ann.txt", quote = FALSE, row.names = FALSE)
 write.table(inf_ps_groups, "INF_PS/inf_ps_groups_wide_file_for_ann.txt", quote = FALSE, row.names = FALSE)
-
