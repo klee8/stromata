b65a70de6fe5a1662b132b2c479f8a0e112b8dd1
klee8 <kate.d.lee@gmail.com>
2019-08-12 14:26:51 +1200

    plain R script for automated cluster permutations

--- Epichloe_clusters/id_DE_clusters/clusters_permutation.R
+++ Epichloe_clusters/id_DE_clusters/clusters_permutation.R
@@ -3,12 +3,12 @@
 
 library(tidyverse)
 
-  
+
 args = commandArgs(trailingOnly=TRUE)
 if (length(args)<1) {
   stop("At least one argument must be supplied: <base_filename>, <number of simulations [default = 10,000]>.n", call.=FALSE)
 } else {
-  infile = paste("../../core_gene_sets/core_genes_", args[1], "_rfmt.txt", sep = "") #| "../core_gene_sets/core_genes_INF_PS_rfmt.txt"
+  infile = paste("../../rfmt_core_gene_sets/core_genes_", args[1], "_rfmt.txt", sep = "") #| "../core_gene_sets/core_genes_INF_PS_rfmt.txt"
   outfile1 = paste(args[1], "_cluster_results.txt", sep = "") #| "observed_clusters.txt"
   outfile2 = paste(args[1], "_permuted_DE_cluster_results.txt", sep = "")   #| "permuted_data.txt"
   graphout = paste(args[1], "_permuted_DE_clusters.pdf", sep = "")
@@ -32,7 +32,7 @@ df <- read.delim(infile, header = TRUE, sep = "\t")
 df <- df %>% distinct
 
 # set DE at 1 for log2FC >= 1 and svalue <= 0.005, set DE at 2 for log2FC >= 2 and svalue <= 0.005
-df <- df %>% mutate(DE = ifelse(((log2fc >= 2) | (log2fc <= -2)) & (svalue_1 <= 0.005), 2, 
+df <- df %>% mutate(DE = ifelse(((log2fc >= 2) | (log2fc <= -2)) & (svalue_1 <= 0.005), 2,
                                         ifelse(((log2fc >= 1) | (log2fc <= -1)) & (svalue_1 <= 0.01), 1 , 0)))
 
 # flag direction of fold change
@@ -44,7 +44,7 @@ df <- df[order(df$species, df$contig, df$start),]
 # identify groups of genes, changing group number when DE changes direction or contig changes
 changes <- function(a, b, x, y) {
   if (is.na(y) | is.na(x) | is.na(a) | is.na(b)) {counter <<- counter + 1}
-  else if (x != y) {counter <<- counter + 1}     
+  else if (x != y) {counter <<- counter + 1}
   else if (a != b) {counter <<- counter + 1}
   return(counter)
 }
@@ -55,15 +55,15 @@ df$change <- mapply(changes, df$contig, df$temp_contig, df$dir_FC, df$temp_dirFC
 df$temp_contig <- NULL
 df$temp_dirFC <- NULL
 
-# identify clusters in each spp by collapsing each group of gene DE results into a string 
+# identify clusters in each spp by collapsing each group of gene DE results into a string
 # assess with regex
 clusters <- data.frame()
 #my_orths <- as.character()
 #my_genes <- as.character()
 for (group in 1:counter) {
   DEstring <- paste(df[df$change == group, c("DE")], collapse = "")
-  #DEorths <- df[df$change == group, c("orthogroup")]  
-  #DEgenes <- df[df$change == group, c("gene_id")] 
+  #DEorths <- df[df$change == group, c("orthogroup")]
+  #DEgenes <- df[df$change == group, c("gene_id")]
   if (grepl("[1,2]+[0]{0,1}[1,2]+", DEstring, perl = TRUE)) {
     temp <- (gregexpr("[1,2]+[0]{0,1}[1,2]+", DEstring, perl = TRUE))
     for (i in temp) {
@@ -77,7 +77,7 @@ for (group in 1:counter) {
         #my_genes <- c(my_genes, genes)
         row <- c(as.numeric(group), as.numeric(clust), as.numeric(pos), as.numeric(len))
         clusters <- rbind(clusters, row)
-      } 
+      }
     }
   }
 }
@@ -105,13 +105,13 @@ for (x in df$change) {
       inclust <- c(inclust, x)
   }
     else { inclust <- c(inclust, 0) }
-  } 
+  }
   else { inclust <- c(inclust, 0) }
   flag <- flag + 1
   prev <- x
 }
 
-# add cluster information to main dataframe  
+# add cluster information to main dataframe
 df$in_cluster <- inclust
 df$clust_pos <- sapply(df$in_cluster, function(x) ifelse( x %in% clusters$group, clusters[clusters$group == x, c("pos")], 0 ))
 df$clust_len <- sapply(df$in_cluster, function(x) ifelse( x %in% clusters$group, clusters[clusters$group == x, c("len")], 0 ))
@@ -146,7 +146,7 @@ for (i in 1:nsim) {
         if ((len > 3) || ( (len == 3) & (grepl('0', clust, perl = TRUE) == FALSE) ) ){
           row <- c(as.numeric(group), as.numeric(clust), as.numeric(pos), as.numeric(len))
           tmp_clusters <- rbind(tmp_clusters, row)
-        } 
+        }
       }
     }
   }
@@ -157,6 +157,6 @@ write.table(num_clusters, outfile2, quote = FALSE, row.names = FALSE, sep = "\t"
 
 ggplot() +
   geom_bar(aes(x = num_clusters)) +
-  geom_vline(xintercept = obs_clusters, color = "red", size=1) 
+  geom_vline(xintercept = obs_clusters, color = "red", size=1)
 
 ggsave(graphout)
