0fc47e8027a170fb92409a9193700881aac4c57d
klee8 <kate.d.lee@gmail.com>
2019-08-12 16:29:20 +1200

    format input file for SMURF

---
+++ Epichloe_clusters/SMURF/format_SMURF_input_files.R
@@ -0,0 +1,68 @@
+# title: "formating_for_SMURF"
+# author: "Kate"
+# date: "5 June 2019"
+# output: html_document
+
+#install.packages("seqinr")
+library("seqinr")
+#install.packages("tidyverse")
+library("tidyverse")
+
+###  read in fasta file and get headers
+elymi_fa <- read.fasta("data/Epichloe_elymi_NfE728.proteins.fa")
+elymi_fa_df <- data.frame(Fragments=names(elymi_fa), Seqs=unlist(getSequence(elymi_fa, as.string=T)))
+elymi_fa_df$gene_id <- gsub("-T1", "", elymi_fa_df$Fragments)
+
+festucae_fa <- read.fasta("data/Epichloe_festucae_E2368.proteins.fa")
+festucae_fa_df <- data.frame(Fragments=names(festucae_fa ), Seqs=unlist(getSequence(festucae_fa , as.string=T)))
+festucae_fa_df$gene_id <- gsub("-T1", "", festucae_fa_df$Fragments)
+
+typhina_fa <- read.fasta("data/Epichloe_typhina_E8.proteins.fa")
+typhina_fa_df <- data.frame(Fragments=names(typhina_fa ), Seqs=unlist(getSequence(typhina_fa , as.string=T)))
+typhina_fa_df$gene_id <- gsub("-T1", "", typhina_fa_df$Fragments)
+
+###   read in gff3 file and get mRNA start and end positions (5'-3')
+elymi_gff3 <- read.delim("data/Epichloe_elymi_NfE728.gff3", header = FALSE, sep = "\t", comment.char = '#')
+elymi_gff3 <- elymi_gff3[elymi_gff3$V3 == "mRNA",]
+elymi_gff3$trans_name <- gsub(";.*", "", elymi_gff3$V9 )
+elymi_gff3$trans_name <- gsub("ID=", "", elymi_gff3$trans_name )
+elymi_gff3$product <- gsub(".*product=", "", elymi_gff3$V9 )
+elymi_gff3$start <- ifelse(elymi_gff3$V7=="+", elymi_gff3$V4, elymi_gff3$V5)
+elymi_gff3$end <- ifelse(elymi_gff3$V7=="+", elymi_gff3$V5, elymi_gff3$V4)
+elymi_gff3 <- elymi_gff3[ , c("trans_name", "V1", "start", "end", "product")]
+colnames(elymi_gff3) <-  c("trans_name", "contig", "start", "end", "product")
+
+write.table(elymi_gff3, "data/E.elymi_ann_for_SMURF.txt", row.names = FALSE, quote = FALSE, sep = "\t")
+
+festucae_gff3 <- read.delim("data/Epichloe_festucae_E2368.gff3", header = FALSE, sep = "\t", comment.char = '#')
+festucae_gff3 <- festucae_gff3[festucae_gff3$V3 == "mRNA",]
+festucae_gff3$trans_name <- gsub(";.*", "", festucae_gff3$V9 )
+festucae_gff3$trans_name <- gsub("ID=", "", festucae_gff3$trans_name )
+festucae_gff3$product <- gsub(".*product=", "", festucae_gff3$V9 )
+festucae_gff3$start <- ifelse(festucae_gff3$V7=="+", festucae_gff3$V4, festucae_gff3$V5)
+festucae_gff3$end <- ifelse(festucae_gff3$V7=="+", festucae_gff3$V5, festucae_gff3$V4)
+festucae_gff3 <- festucae_gff3[ , c("trans_name", "V1", "start", "end", "product")]
+colnames(festucae_gff3) <-  c("trans_name", "contig", "start", "end", "product")
+
+write.table(festucae_gff3, "data/E.festucae_ann_for_SMURF.txt", row.names = FALSE, quote = FALSE, sep = "\t")
+
+typhina_gff3 <- read.delim("data/Epichloe_typhina_E8.gff3", header = FALSE, sep = "\t", comment.char = '#')
+typhina_gff3 <- typhina_gff3[typhina_gff3$V3 == "mRNA",]
+typhina_gff3$trans_name <- gsub(";.*", "", typhina_gff3$V9 )
+typhina_gff3$trans_name <- gsub("ID=", "", typhina_gff3$trans_name )
+typhina_gff3$product <- gsub(".*product=", "", typhina_gff3$V9 )
+typhina_gff3$start <- ifelse(typhina_gff3$V7=="+", typhina_gff3$V4, typhina_gff3$V5)
+typhina_gff3$end <- ifelse(typhina_gff3$V7=="+", typhina_gff3$V5, typhina_gff3$V4)
+typhina_gff3 <- typhina_gff3[ , c("trans_name", "V1", "start", "end", "product")]
+colnames(typhina_gff3) <-  c("trans_name", "contig", "start", "end", "product")
+
+write.table(typhina_gff3, "data/E.typhina_ann_for_SMURF.txt", row.names = FALSE, quote = FALSE, sep = "\t")
+
+#str(elymi_fa_df)
+#str(elymi_gff3)
+
+#str(festucae_fa_df)
+#str(festucae_gff3)
+
+#str(typhina_fa_df)
+#str(typhina_gff3)
