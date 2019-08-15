8015a2704bbc24b5267a5fc65588024f9728847d
klee8 <kate.d.lee@gmail.com>
2019-08-15 15:24:15 +1200

    changed dir name

--- Epichloe_clusters/id_DE_clusters/run_DE_and_cluster_permutations.sh
+++ Epichloe_clusters/id_DE_clusters/run_DE_and_cluster_permutations.sh
@@ -13,24 +13,24 @@
 #!usr/bin/bash
 
 
-SCRIPTDIR="/media/kate/Massey_linux_onl/projects/STROMATA/stromata_analysis/Epichloe_clusters/id_DE_clusters"
-RESDIR="/media/kate/Massey_linux_onl/projects/STROMATA/results/Epichloe_clusters/id_DE_clusters"
+SCRIPTDIR="/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters"
+RESDIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/id_DE_clusters"
 
 cd $RESDIR
 
 #####   DE permutations
 cd STR_PS
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_PS 1000
-cd ../INF_PS
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R INF_PS 1000
-cd ../STR_INF
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_INF 1000
+#Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_PS 1000000
+#cd ../INF_PS
+#Rscript --vanilla $SCRIPTDIR/DE_permutations.R INF_PS 1000000
+#cd ../STR_INF
+#Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_INF 1000000
 
 #####   Cluster permutations
-cd ../STR_PS
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_PS 1000
-cd ../INF_PS
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R INF_PS 1000
+#cd ../STR_PS
+#Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_PS 1000000
+#cd ../INF_PS
+#Rscript --vanilla $SCRIPTDIR/clusters_permutation.R INF_PS 1000000
 cd ../STR_INF
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_INF 1000
+Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_INF 1000000
 cd ..
