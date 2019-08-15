8097e680a29ed72f8c3e3ee57b9c3de2c3653064
klee8 <kate.d.lee@gmail.com>
2019-08-15 15:24:32 +1200

    changed dir name

--- Epichloe_clusters/id_DE_clusters/run_reformat_core_gene_set.sh
+++ Epichloe_clusters/id_DE_clusters/run_reformat_core_gene_set.sh
@@ -1,7 +1,7 @@
 #!ust/bin/bash
 
-DATADIR="/media/kate/Massey_linux_onl/projects/STROMATA/stromata_analysis/Epichloe_stromata_DE/3_DESeq2/"
-RESDIR="/media/kate/Massey_linux_onl/projects/STROMATA/results/Epichloe_clusters/rfmt_core_gene_sets/"
+DATADIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_stromata_DE/3_DESeq2/"
+RESDIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/rfmt_core_gene_sets/"
 
 SCRIPTDIR="/media/kate/Massey_linux_onl/projects/STROMATA/stromata_analysis/Epichloe_clusters/rfmt_core_gene_sets/"
 
