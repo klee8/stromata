105f07e0aaa1ddd245c8cca4703ca5572aca6a57
klee8 <kate.d.lee@gmail.com>
2019-09-18 14:28:50 +1200

    output genetoGOmapping

--- Epichloe_clusters/id_DE_clusters/run_cluster_permutations.sh
+++ Epichloe_clusters/id_DE_clusters/run_cluster_permutations.sh
@@ -8,7 +8,7 @@
 #!usr/bin/bash
 
 
-FUNPATH="/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters/DE_fun.R"
+SCRIPTDIR="/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters"
 DATADIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/rfmt_core_gene_sets/"
 RESDIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/id_DE_clusters/"
 
@@ -17,10 +17,10 @@ cd $RESDIR
 mkdir STR_PS INF_PS STR_INF
 
 #####   Cluster permutations
-cd ../STR_PS
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_PS 10000 $FUNPATH $DATADIR $RESDIR
+cd STR_PS
+Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_PS 10000 $SCRIPTDIR/cluster_fun.R $DATADIR $RESDIR
 cd ../INF_PS
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R INF_PS 10000 $FUNPATH $DATADIR $RESDIR
+Rscript --vanilla $SCRIPTDIR/clusters_permutation.R INF_PS 10000 $SCRIPTDIR/cluster_fun.R $DATADIR $RESDIR
 cd ../STR_INF
-Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_INF 10000 $FUNPATH $DATADIR $RESDIR
+Rscript --vanilla $SCRIPTDIR/clusters_permutation.R STR_INF 10000 $SCRIPTDIR/cluster_fun.R $DATADIR $RESDIR
 cd ..
