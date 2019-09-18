3543a46c18a9dcb2b32ae21b4059135fb0135702
klee8 <kate.d.lee@gmail.com>
2019-09-18 14:27:20 +1200

    changed script dir

--- Epichloe_clusters/id_DE_clusters/run_DE_permutations.sh
+++ Epichloe_clusters/id_DE_clusters/run_DE_permutations.sh
@@ -8,7 +8,7 @@
 #!usr/bin/bash
 
 
-FUNPATH="/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters/DE_fun.R"
+SCRIPTDIR="/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters"
 DATADIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/rfmt_core_gene_sets/"
 RESDIR="/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/id_DE_clusters/"
 
@@ -18,9 +18,9 @@ mkdir STR_PS INF_PS STR_INF
 
 #####   DE permutations
 cd STR_PS
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_PS 10000 $FUNPATH $DATADIR $RESDIR
+Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_PS 10000 $SCRIPTDIR/DE_fun.R $DATADIR $RESDIR
 cd ../INF_PS
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R INF_PS 10000 $FUNPATH $DATADIR $RESDIR
+Rscript --vanilla $SCRIPTDIR/DE_permutations.R INF_PS 10000 $SCRIPTDIR/DE_fun.R $DATADIR $RESDIR
 cd ../STR_INF
-Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_INF 10000 $FUNPATH $DATADIR $RESDIR
+Rscript --vanilla $SCRIPTDIR/DE_permutations.R STR_INF 10000 $SCRIPTDIR/DE_fun.R $DATADIR $RESDIR
 
