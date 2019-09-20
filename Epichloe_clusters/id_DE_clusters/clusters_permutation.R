8000dd55cc0aeeaea68b169cb1f0cd1fbb1ca7f0
klee8 <kate.d.lee@gmail.com>
2019-09-20 15:01:39 +1200

    added print input to logs

--- Epichloe_clusters/id_DE_clusters/clusters_permutation.R
+++ Epichloe_clusters/id_DE_clusters/clusters_permutation.R
@@ -9,11 +9,14 @@ library(data.table)
 ###############################################################
 #           GET FILENAMES AND NUMBER OF SIMULATIONS NEEDED
 ###############################################################
-
-#resdir <- "/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/id_DE_clusters/STR_PS/"
-#scriptdir <- "/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters/"
+#basefilename <- "STR_PS"
+#nsim <- 10000
+#functionfile <- "/media/kate/Massey_linux_onl/projects/stromata_analysis/Epichloe_clusters/id_DE_clusters/cluster_fun.R"
+#datadir <- "/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/rfmt_core_gene_sets/"
+#resdir <- "/media/kate/Massey_linux_onl/projects/results/stromata/Epichloe_clusters/id_DE_clusters/"
+#args <- c(basefilename, nsim, functionfile, datadir, resdir)
 #setwd(resdir)
-#args = c("STR_PS", 10, "cluster_fun.R")
+
 
 args = commandArgs(trailingOnly=TRUE)
 dir.create("cluster_perm_results")
@@ -32,8 +35,10 @@ if (length(args)<1) {
   sum_perm_file = paste("cluster_perm_results/", basefilename, "_permuted_cluster_summary.txt", sep = "")
   graphout_file = paste("cluster_perm_results/", basefilename, "_permuted_DE_clusters.pdf", sep = "")
 }
+checks <- c(basefilename, nsim, functionfile, datadir, resdir)
+print(checks)
 
-setwd(resdir)
+#setwd(paste(resdir, basefilename, sep = ""))
 source(functionfile)
 
 # read in reformatted data
@@ -50,17 +55,21 @@ clusters <- find_assessment_blocks_with_clusters(df)
 
 # put cluster information back into main data frame
 df <- genes_in_clusters(df, clusters)
-head(df[df$clust_len != 0,])
+#head(df[df$clust_len != 0,])
 
 # use cluster-annotated df to summarise cluster information
 cluster_sum <- sum_clusters(df)
-head(cluster_sum)
+#head(cluster_sum)
 write.table(cluster_sum, cluster_res_file, row.names = FALSE, quote = FALSE, sep = "\t")
 
 # number of observed clusters
 obs_clusters <- nrow(clusters)
 
+# check for shared genes among clusters
+df <- shared_cluster_genes(df)
+
 # write out python scripts to graph clusters
+dir.create("Rgraphs")
 make_pygraphs(df)
 
 # run permutation function with foreach parellisation
