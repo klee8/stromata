ae3594d278c757637ff1c666060a3cb448a432fa
klee8 <kate.d.lee@gmail.com>
2020-10-16 15:36:13 +1300

    changed gene list

--- stromata_comparative_genomics/blastn/summarise_blast_bitscores.sh
+++ stromata_comparative_genomics/blastn/summarise_blast_bitscores.sh
@@ -6,11 +6,15 @@ for i in `cat genome_tag_list.txt`; do rm $i/*bestaln.txt; done
 rm all_bitscores.txt
 
 # get the best hits
-for i in `cat genome_tag_list.txt`; do for j in `cat elymi_core_gene_names.txt`; do grep -m 1 $j $i/$i.aln.txt >> $i/$i.bestaln.txt ; done; done 
+for i in `cat genome_tag_list.txt`; 
+# do for j in `cat elymi_core_gene_names.txt`; 
+  do for j in `cat control_gene_list.txt`
+    do grep -m 1 $j $i/$i.aln.txt >> $i/$i.bestaln.txt ; done; done 
 
 # paste the bitscores into one file
 cp elymi_core_gene_names.txt all_bitscores.txt
 for i in `cat genome_tag_list.txt`; do cut -f 12 $i/$i.bestaln.txt > tmp1; paste all_bitscores.txt tmp1 > tmp2; mv tmp2 all_bitscores.txt; done
+
 echo -ne "elymi_core_genes\t" > headerline 
 for i in `cat genome_tag_list.txt`; do echo -ne "$i\t" >> headerline; done; echo >> headerline
 cat headerline all_bitscores.txt > temp
