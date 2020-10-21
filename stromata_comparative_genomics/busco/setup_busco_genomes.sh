58d91ceece26234bda69d6837e9411c209193c24
klee8 <kate.d.lee@gmail.com>
2020-10-21 15:19:56 +1300

    re-arranged folder structure

--- stromata_comparative_genomics/busco/setup_busco_genomes.sh
+++ stromata_comparative_genomics/busco/setup_busco_genomes.sh
@@ -4,21 +4,21 @@
 
 for i in `cat genome_tag_list.txt`
   do
-    mkdir results/$i
-    cp ../exonerate/align/$i/$i.masked.fa results/$i
+    mkdir genome_results/$i
+    cp ../exonerate/align/$i/$i.masked.fa genome_results/$i
 
     # setup configuration file
-    cp myconfig.ini results/$i
-    sed -i "s/\/path\/to\/input_file.fna/proteins\/TEMP.fna/g" results/$i/myconfig.ini  
-    sed -i "s/\/path\/to\/output_folder/results\/TEMP/g" results/$i/myconfig.ini
-    sed -i "s/BUSCO_run/busco_TEMP/g" results/$i/myconfig.ini
-    sed -i "s/cpu = 16/cpu = 12/g" results/$i/myconfig.ini
-    sed -i "s/lineage_dataset = bacteria/lineage_dataset = eukaryota_odb10/g" results/$i/myconfig.ini
-    sed -i "s/TEMP/$i/" results/$i/myconfig.ini
+    cp myconfig.ini genome_results/$i
+    sed -i "s/\/path\/to\/input_file.fna/proteins\/TEMP.fna/g" genome_results/$i/myconfig.ini  
+    sed -i "s/\/path\/to\/output_folder/results\/TEMP/g" genome_results/$i/myconfig.ini
+    sed -i "s/BUSCO_run/busco_TEMP/g" genome_results/$i/myconfig.ini
+    sed -i "s/cpu = 16/cpu = 12/g" genome_results/$i/myconfig.ini
+    sed -i "s/lineage_dataset = bacteria/lineage_dataset = eukaryota_odb10/g" genome_results/$i/myconfig.ini
+    sed -i "s/TEMP/$i/" genome_results/$i/myconfig.ini
 
     # setup docker run script
 
-    echo "docker run -u $(id -u) -v $(pwd):/results/$i -w /results/$i/ ezlabgva/busco:v4.1.1_cv1 busco -i results/$i/$i.masked.fa --config=myconfig.ini --out $i --mode genome --lineage eukaryota_odb10 -f" >> run_busco_docker.sh 
+    echo "docker run -u $(id -u) -v $(pwd):/genome_results/$i -w /genome_results/$i/ ezlabgva/busco:v4.1.1_cv1 busco -i genome_results/$i/$i.masked.fa --config=myconfig.ini --out $i --mode genome --lineage eukaryota_odb10 -f" >> run_busco_docker.sh 
 
 done
 
@@ -26,7 +26,6 @@ done
 # wget "https://gitlab.com/ezlab/busco/-/commit/b8cfb1e1892a4bb08a3c0ee34ddcad1f75d87307"
 # and unpack
 echo "mkdir BUSCO_summaries" >> run_busco_docker.sh
-echo "for i in `cat genome_tag_list.txt`; do cp $i/short_summary.specific*.txt  BUSCO_summaries; done" >> run_busco_docker.sh
+for i in `cat genome_tag_list.txt`;
+ do echo "cp genome_results/$i/short_summary.specific*.txt  BUSCO_summaries; done" >> run_busco_docker.sh; done
 echo "python busco-4.1.2/scripts/generate_plot.py -wd BUSCO_summaries/" >> run_busco_docker.sh
-
-
--- stromata_comparative_genomics/busco/setup_busco_proteins.sh
+++ stromata_comparative_genomics/busco/setup_busco_proteins.sh
@@ -11,14 +11,23 @@ for i in `cat genome_tag_list.txt`
     sed -i "s/\/path\/to\/input_file.fna/proteins\/TEMP.fna/g" results/$i/myconfig.ini  
     sed -i "s/\/path\/to\/output_folder/results\/TEMP/g" results/$i/myconfig.ini
     sed -i "s/BUSCO_run/busco_TEMP/g" results/$i/myconfig.ini
-    sed -i "s/mode = genome/mode = protein/g" results/$i/myconfig.ini
+    sed -i "s/mode = genome/mode = proteins/g" results/$i/myconfig.ini
     sed -i "s/cpu = 16/cpu = 12/g" results/$i/myconfig.ini
+    sed -i "s/lineage_dataset = bacteria/lineage_dataset = eukaryota_odb10/g" results/$i/myconfig.ini
     sed -i "s/TEMP/$i/" results/$i/myconfig.ini
 
     # setup docker run script
-    echo "docker run -u $(id -u) -v $(pwd):/results/$i -w /results/$i/ ezlabgva/busco:v4.1.1_cv1 busco -i proteins/$i.fna --config=results/$i/myconfig.ini --out $i --mode proteins --lineage eukaryota_odb10 -f" >> run_busco_docker.sh
+    
+    echo "docker run -u $(id -u) -v $(pwd):/results/$i -w /results/$i/ ezlabgva/busco:v4.1.1_cv1 busco -i proteins/$i.fna --config=results/$i/myconfig.ini --out $i --mode proteins --lineage eukaryota_odb10 -f" >> run_busco_docker.sh 
+    #echo "docker run -u $(id -u) -v $(pwd):/results/$i -w /results/$i/ ezlabgva/busco:v4.1.1_cv1 busco --config=results/$i/myconfig.ini --lineage eukaryota_odb10 -f" >> run_busco_docker.sh
 
 done
 
+# summarise results (note requires a local busco download).
+# wget "https://gitlab.com/ezlab/busco/-/commit/b8cfb1e1892a4bb08a3c0ee34ddcad1f75d87307"
+# and unpack
+echo "mkdir BUSCO_summaries" >> run_busco_docker.sh
+echo "for i in `cat genome_tag_list.txt`; do cp $i/short_summary.specific*.txt  BUSCO_summaries; done" >> run_busco_docker.sh
+echo "python busco-4.1.2/scripts/generate_plot.py -wd BUSCO_summaries/" >> run_busco_docker.sh
 
 
